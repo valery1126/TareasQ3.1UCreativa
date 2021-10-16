5cfb35d2937e8ed24b9dc87eb02ef8f76f057840
valery1126 <valerymvc26@gmail.com>
2021-10-15 20:57:05 -0600

    Tarea 2 Valery Campos

--- Tarea2/cypress/integration/Tests/ejercicio4.spec.js
+++ Tarea2/cypress/integration/Tests/ejercicio4.spec.js
@@ -8,15 +8,18 @@ describe('Ejecicio 4 Tests', () => {
   
     context('Ejercicio 4', () => {
       
-      it('Popover', () => {
+     it('Popover', () => {
 
         cy.get('#actions > div > div:nth-child(13) > div > button').scrollIntoView();
-        cy.get('#actions > div > div:nth-child(13) > div > button').click();
+        cy.get('.action-btn').click();
           
         cy.url().should('include', '/'); 
 
+        cy.get('.popover-title').scrollIntoView();
+        cy.get('.popover-title').should('be.visible');
         
       });
+
 
       it('Canvas', () => {
         
@@ -29,22 +32,31 @@ describe('Ejecicio 4 Tests', () => {
         cy.get('#action-canvas').click(170,165);   
           
         cy.url().should('include', '/'); 
+
         
       });
+
 
       it('Multiple clicks', () => {
 
        cy.get('.action-labels > span').click({multiple:true});
   
         cy.url().should('include', '/'); 
+
+        cy.get('.popover-content').should('be.visible',{multiple:true});
         
       });
+
+
 
       it('Force', () => {
 
         cy.get('#actions > div > div:nth-child(13) > div > div.action-opacity > button').click({force:true});
           
         cy.url().should('include', '/'); 
+
+        cy.get('.popover-content').scrollIntoView();
+        cy.get('.popover-content').should('be.visible');
         
       });
 
